e21d41261bfc259e17015b3d82dc2f17bc00fbde
Lena Medina <lena.medina@ymail.com>
2019-05-30 16:13:25 -0400

    Fixed JavaScript and added postFullName to CSS file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function idCard(){
-    var first = document.getElementById('firstName').value;
-    var last = document.getElementById('lastName').value;
-    var address = document.getElementById('address').value;
-    var age = parseInt(document.getElementById('age').value);
-    var phone = parseInt(document.getElementById('phoneNumber').value);
-    var FullName = first + " " + last;
+    var first = document.getElementById("firstName").value;
+    var last = document.getElementById("lastName").value;
+    var address = document.getElementById("address").value;
+    var age = parseInt(document.getElementById("age").value);
+    var phone = parseInt(document.getElementById("phoneNumber").value);
+    var fullName = first + " " + last;
 
-    document.getElementById('postFullName').innerHTML = FullName;
-    document.getElementById('postAddress').innerHTML = address;
+    document.getElementById("postFullName").innerHTML = fullName;
+    document.getElementById("postAddress").innerHTML = address;
 
     var numberArray = []
     numberArray.push(age,phone)
 
     for(var i=0; i < numberArray.length; i++){
-        if (numberArray[i])<100){
+        if (numberArray[i]<100){
             document.getElementById("postAge").innerHTML = "Age: " + numberArray[i];  
         }else if(numberArray[i]>=100){
             document.getElementById("postPhoneNumber").innerHTML ="Phone Number: " + numberArray[i];
