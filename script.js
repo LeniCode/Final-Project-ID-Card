db6a39b0ebfd0558aea68de3e838ab1f54a39067
Lena Medina <lena.medina@ymail.com>
2019-05-30 14:22:30 -0400

    Fixed JavaScript according to tutorial
    But it still doesn't work

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function idCard() {
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var postFullName = firstName + "" + lastName;
-    document.getElementByID('postFullName').innerHTML = postFullName;
-    
+function idCard(){
+    var first = document.getElementById('firstName').value;
+    var last = document.getElementById('lastName').value;
     var address = document.getElementById('address').value;
-    document.getElementByID('postAddress').innerHTML = postAddress;
-    
-    var age = document.getElementById('age').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
+    var age = parseInt(document.getElementById('age').value);
+    var phone = parseInt(document.getElementById('phoneNumber').value);
+    var FullName = first + " " + last;
+
+    document.getElementByID('postFullName').innerHTML = FullName;
+    document.getElementByID('postAddress').innerHTML = address;
+
     var numberArray = []
+    numberArray.push(age,phone)
+
+    for(var i=0; i < numberArray.length; i++){
+        if (numberArray[i]),100){
+            document.getElementById("postAge").innerHTML = "Age: " + numberArray[i];  
+        }else if(numberArray[i]>=100){
+            document.getElementById("postPhoneNumber").innerHTML ="Phone Number: " + numberArray[i];
+        }
+
+    }
   }
